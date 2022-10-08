dc389085d80bbd4201e61ba232a131a4c0373a4f
Luisa Fernanda Barragan Avila <luisaavila304@gmail.com>
2022-10-08 18:50:11 -0500

    enrutamiento login,register,home

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,26 @@
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AuthRoutingModule {}
